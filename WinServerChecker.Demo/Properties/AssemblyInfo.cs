7925d4214dede7575ee5f6da80a8c729292cf358
Joe Harrison <email@joeharrison.com>
2016-02-27 14:02:20 -0500

    renamed demo project

--- WinServerChecker.Demo/Properties/AssemblyInfo.cs
+++ WinServerChecker.Demo/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinServerChecker.Web")]
+[assembly: AssemblyTitle("WinServerChecker.Demo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
